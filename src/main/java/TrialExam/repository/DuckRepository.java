e36d0cad81b955fe807067f8acb6e944800a3efb
Kim <kim.houweling@live.nl>
2019-06-12 14:47:55 +0200

    changes

--- src/main/java/TrialExam/repository/DuckRepository.java
+++ src/main/java/TrialExam/repository/DuckRepository.java
@@ -2,9 +2,13 @@ package TrialExam.repository;
 
 import TrialExam.model.Duck;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface DuckRepository extends JpaRepository<Duck, Long> {
+//    @Query("select s.quantity * g.price from Stock s, Guitar g where s.guitar.id = g.id and s.guitar.id = ?1")
+//    public int getStockValueByGuitarId(Long id);
+
 }
